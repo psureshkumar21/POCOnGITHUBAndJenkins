5f0d08013caddc3f8d88af86c85a2f5cfa1b41fb
suresh <suresh@suresh-VirtualBox>
2016-04-28 16:33:19 +0530

    FirstCommit

---
+++ src/SamplePackage/FirstJavaProgram.java
@@ -0,0 +1,13 @@
+package SamplePackage;
+
+public class FirstJavaProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is my first Java Program");
+
+	}
+
+}
